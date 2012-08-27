08e7425e20a37d4b015641707ba6e15d99484a81
Daniel <robotright@gmail.com>
2012-08-27 17:49:19 +0800

    add parsed/normalized data to database

--- website/app/controllers/pages_controller.rb
+++ website/app/controllers/pages_controller.rb
@@ -14,6 +14,26 @@ class PagesController < ApplicationController
 
       purchase_details = CSV.read(file_path, {:headers => :first_row, :col_sep => "\t" })
 
+      # add data
+      purchase_details.each do |row|
+        customer_name = row['purchaser name']
+        item_description = row['item description']
+        item_price = row['item price']
+        purchase_count = row['purchase count']
+        merchant_name = row['merchant name']
+        merchant_address = row['merchant address']
+
+        merchant = Merchant.find_or_create_by_name_and_address!(name:merchant_name, address:merchant_address)
+        item = merchant.items.find_or_create_by_description!(description:item_description, price:item_price)
+        customer = Customer.find_or_create_by_name!(name:customer_name)
+
+        Purchase.create(count:purchase_count).tap do |purchase|
+          purchase.customer_id = customer.id
+          purchase.item_id = item.id
+          purchase.save!
+        end
+      end
+
       total = purchase_details.map { |row| row['item price'].to_i * row['purchase count'].to_i }.sum
 
       flash[:notice] = "file uploaded. total revenue = $#{total}"
