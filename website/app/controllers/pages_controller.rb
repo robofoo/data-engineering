5ea161a513640c2b946fa9a03fa282fe03759e58
Daniel <robotright@gmail.com>
2012-08-27 17:24:42 +0800

    upload files

--- website/app/controllers/pages_controller.rb
+++ website/app/controllers/pages_controller.rb
@@ -4,6 +4,11 @@ class PagesController < ApplicationController
 
   def upload
     if params.has_key?(:purchases)
+      uploaded_io = params[:purchases]
+      File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
+        file.write(uploaded_io.read)
+      end
+
       flash[:notice] = 'file uploaded'
     else
       flash[:notice] = 'please select a file'
